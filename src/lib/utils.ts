961c0645a92915e31ad2a2a4315a1b2847a9f321
marawanthedev <61912691+marawanthedev@users.noreply.github.com>
2024-08-24 01:19:43 +0800

    feature: Complete landing page and Auth

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,6 @@
-import clsx, { ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-    // px-2 and py-2 will result in p-2 with twMerge
-    // clsx helps in handling conflicts if the classes , example bg-black bg-white
-    return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs))
 }
