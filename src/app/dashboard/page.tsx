261245c573ef18209d21b34dab0bd1a364914184
marawanthedev <61912691+marawanthedev@users.noreply.github.com>
2024-08-25 00:40:26 +0800

    infra: add prisma DB

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,9 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { KindeUser } from "@kinde-oss/kinde-auth-nextjs/types";
 import { redirect } from "next/navigation";
 
 const Page = async () => {
   const { getUser } = getKindeServerSession();
-  const user: KindeUser | null = await getUser();
+  const user = await getUser();
 
   if (!user) {
     redirect("/auth-callback?origin=dashboard");
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -2,16 +2,33 @@ import { z } from 'zod';
 import { procedure, router } from './trpc';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { TRPCError } from '@trpc/server';
+import { db } from '@/db';
 
 export const appRouter = router({
-    authCallBack: procedure.query(() => {
+    authCallBack: procedure.query(async () => {
         const { getUser } = getKindeServerSession()
         const user = getUser()
-        if (!user || !user.id) {
+        if (!user || !user.id || !user.email) {
             throw new TRPCError({ code: "UNAUTHORIZED" })
         }
 
         // check if user is in db
+        const dbUser = await db.user.findFirst({
+            where: {
+                email: user.email
+            }
+        })
+
+
+        if (!dbUser) {
+            // create user in database
+            await db.user.create({
+                data: {
+                    id: user.id,
+                    email: user.email,
+                }
+            })
+        }
 
         return { success: true }
     })
